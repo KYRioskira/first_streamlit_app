16a4d57c8ffab1502edc6e14de704a531b8b1132
KYRioskira <143253713+KYRioskira@users.noreply.github.com>
2023-08-26 14:18:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title("Never Fold Never Backdown")
   
 streamlit.header('🥣Breakfast Menu')
@@ -7,3 +9,7 @@ streamlit.text('🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🐔Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
